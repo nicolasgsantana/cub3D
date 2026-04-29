807865474d60aeec40a41ce039deb18e947d0f11
Alessandro Tude Da Rocha Oliveira <aletude-@c1r6p2.42sp.org.br>
2026-04-29 16:07:38 -0300

    struct map

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aletude- <aletude-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 14:08:45 by nde-sant          #+#    #+#             */
-/*   Updated: 2026/04/27 14:09:40 by nde-sant         ###   ########.fr       */
+/*   Updated: 2026/04/29 15:59:00 by aletude-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,26 @@
 
 # include "libft.h"
 # include "MLX42.h"
+
+typedef struct s_map
+{
+	char		**map_grid;
+	int			width;
+	int			height;
+
+	int			floor_color;
+	int			ceiling_color;
+
+	char		*north_texture;
+	char		*south_texture;
+	char		*east_texture;
+	char		*west_texture;
+
+/*	t_texture	north;
+	t_texture	east;
+	t_texture	west;
+	t_texture	south; */
+}				t_map;
+
 
 #endif
