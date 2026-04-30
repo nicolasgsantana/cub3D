38e188742547bac94bdb47ede8df122e6d245e09
nicolasgsantana <nicolas07san@gmail.com>
2026-04-30 11:37:34 -0300

    fix: map now scales based on map and img size

--- src/minimap/render_map.c
+++ src/minimap/render_map.c
@@ -1,39 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   render.c                                           :+:      :+:    :+:   */
+/*   render_map.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/29 16:52:50 by nde-sant          #+#    #+#             */
-/*   Updated: 2026/04/29 18:27:35 by nde-sant         ###   ########.fr       */
+/*   Updated: 2026/04/30 11:30:58 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 #include "settings.h"
-#include "math.h"
+#include "minimap.h"
 
-void	fill_block(mlx_image_t **img, int posX, int posY, int wall)
+void	fill_block(mlx_image_t **img, t_map *map, t_vector pos, int wall)
 {
 	int	x;
 	int	y;
 	int	size;
 
-	size = 10;
-	ft_printf("%d\n", size);
-	y = 0;
+	size = (*img)->width / fmax(map->width, map->height);
+	y = 1;
 	while (y < size)
 	{
-		x = 0;
+		x = 1;
 		while (x < size)
 		{
 			if (wall)
-				mlx_put_pixel(*img, (posX * size) + x,
-					(posY * size) + y, MMAP_FG_COLOR);
+				mlx_put_pixel(*img, (pos.x * size) + x - 1,
+					(pos.y * size) + y - 1, MMAP_FG_COLOR);
 			else
-				mlx_put_pixel(*img, (posX * size) + x,
-					(posY * size) + y, MMAP_BG_COLOR);
+				mlx_put_pixel(*img, (pos.x * size) + x - 1,
+					(pos.y * size) + y - 1, MMAP_BG_COLOR);
 			x++;
 		}
 		y++;
@@ -42,8 +41,9 @@ void	fill_block(mlx_image_t **img, int posX, int posY, int wall)
 
 void	render_minimap(t_map *map, mlx_image_t **img)
 {
-	int	row;
-	int	col;
+	int			row;
+	int			col;
+	t_vector	pos;
 
 	row = 0;
 	while (row < map->height)
@@ -51,10 +51,12 @@ void	render_minimap(t_map *map, mlx_image_t **img)
 		col = 0;
 		while (col < map->width)
 		{
+			pos.x = col;
+			pos.y = row;
 			if (map->map_grid[row][col] == '0')
-				fill_block(img, col, row, 0);
+				fill_block(img, map, pos, 0);
 			else
-				fill_block(img, col, row, 1);
+				fill_block(img, map, pos, 1);
 			col++;
 		}
 		row++;
