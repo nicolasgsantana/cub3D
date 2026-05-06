08ab36c6f9cc0ca25e23a01a8aca5e7e829d30b1
nicolasgsantana <nicolas07san@gmail.com>
2026-05-06 14:47:36 -0300

    refactor: make fill block reusable

--- src/minimap/render_map.c
+++ src/minimap/render_map.c
@@ -3,59 +3,57 @@
 /*                                                        :::      ::::::::   */
 /*   render_map.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alessandro <alessandro@student.42.fr>      +#+  +:+       +#+        */
+/*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/29 16:52:50 by nde-sant          #+#    #+#             */
-/*   Updated: 2026/05/05 12:12:51 by alessandro       ###   ########.fr       */
+/*   Updated: 2026/05/06 14:42:34 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
 
-void	fill_block(mlx_image_t **img, t_map *map, t_point pos, int wall)
+void	fill_block(t_game *game, t_point pos, int size, uint32_t color)
 {
-	int	x;
-	int	y;
-	int	size;
+	int			x;
+	int			y;
+	mlx_image_t	*img;
 
-	size = (*img)->width / fmax(map->width, map->height);
+	img = game->scene_img;
 	y = 1;
 	while (y < size)
 	{
 		x = 1;
 		while (x < size)
 		{
-			if (wall)
-				mlx_put_pixel(*img, (pos.x * size) + x - 1,
-					(pos.y * size) + y - 1, MMAP_FG_COLOR);
-			else
-				mlx_put_pixel(*img, (pos.x * size) + x - 1,
-					(pos.y * size) + y - 1, MMAP_BG_COLOR);
+			mlx_put_pixel(img, pos.x + x - 1,
+				pos.y + y - 1, color);
 			x++;
 		}
 		y++;
 	}
 }
 
-void	render_minimap(t_map *map, mlx_image_t **img)
+void	render_minimap(t_game *game)
 {
 	int			row;
 	int			col;
+	int			size;
 	t_point		pos;
 
+	size = WIN_WIDTH / fmax(game->map.width, game->map.height);
 	row = 0;
-	while (row < map->height)
+	while (row < game->map.height)
 	{
 		col = 0;
-		while (col < map->width)
+		while (col < game->map.width)
 		{
-			pos.x = col;
-			pos.y = row;
-			if (map->map_grid[row][col] == '0')
-				fill_block(img, map, pos, 0);
+			pos.x = col * size;
+			pos.y = row * size;
+			if (game->map.map_grid[row][col] == '1')
+				fill_block(game, pos, size, MMAP_FG_COLOR);
 			else
-				fill_block(img, map, pos, 1);
+				fill_block(game, pos, size, MMAP_BG_COLOR);
 			col++;
 		}
 		row++;
